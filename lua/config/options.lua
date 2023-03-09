03de55aad7bd1a9a5ca20a5e224ede540ef9c0a1
Raza <contact@raza.gr>
2023-03-09 06:33:26 +0100

    select comment text

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- vim.opt.winbar = "%=%m %f"
-vim.opt.relativenumber = false -- Relative line numbers
-vim.opt.number = true -- Line numbers
+-- vim.opt.relativenumber = false -- Relative line numbers
+vim.opt.number = true -- Line number
 vim.opt.cursorline = true -- Highlight current lines
--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,20 +1,15 @@
 return {
   {
-    "numToStr/Comment.nvim",
-    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-    opts = function()
-      return {
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      }
-    end,
-    -- opts = {
-    --   context_commentstring = {
-    --     enable = true,
-    --     enable_autocmd = false,
-    --   },
-    --   pre_hook = function()
-    --     return require("ts_context_commentstring.internal").create_pre_hook()
-    --   end,
-    -- },
+    "echasnovski/mini.comment",
+    opts = {
+      hooks = {
+        pre = function()
+          require("ts_context_commentstring.internal").update_commentstring({})
+        end,
+        post = function()
+          vim.cmd("norm! gv")
+        end,
+      },
+    },
   },
 }
